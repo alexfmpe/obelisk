6805c3f4e88169106548717e1792a10141e078db
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-06-02 04:25:19 +0100

    moar instances

--- skeleton/frontend/src/Frontend.hs
+++ skeleton/frontend/src/Frontend.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
@@ -9,16 +10,14 @@ module Frontend where
 
 import Control.Applicative
 import Control.Arrow
+import Control.Category
+import Control.Monad hiding (join)
 import Data.Align
 import Data.Functor.Alt
-import Data.Functor.Apply
 import Data.Functor.Bind
 import Data.Semigroup
 import Data.Semigroupoid
-import Control.Category
-import Control.Monad
 import Data.Profunctor (Profunctor(..))
-import Data.These
 import qualified Data.Text as T
 import Prelude hiding (id, (.))
 
@@ -40,6 +39,11 @@ frontend = Frontend
       elAttr "img" ("src" =: static @"obelisk.jpg") blank
   }
 
+-- TODO:
+--- check laws
+--- relax/simplify constraints as much as possible
+---- remove redundant 'mkStep'
+--- consider efficient overrides
 newtype Step t m a b = Step (a -> m (Event t b)) deriving Functor
 
 mkStep :: MonadHold t m => (a -> m (Event t b)) -> Step t m a b
@@ -47,6 +51,8 @@ mkStep a2b = Step $ headE <=< a2b
 
 dup :: a -> (a,a)
 dup a = (a,a)
+
+
 
 instance (Adjustable t m, MonadHold t m) => Semigroupoid (Step t m) where
   Step b2c `o` Step a2b = mkStep $
@@ -64,8 +70,17 @@ instance (Adjustable t m, MonadHold t m, PostBuild t m) => Arrow (Step t m) wher
     ffor2 (holdJust =<< a2b a) (holdJust =<< c2d c) $ \b d ->
       fmapMaybe id $ updated $ (liftA2 . liftA2) (,) b d
 
+instance (Adjustable t m, MonadHold t m, PostBuild t m) => ArrowChoice (Step t m) where
+  Step a2b +++ Step c2d = mkStep $ \case
+    Left a -> fmap Left <$> a2b a
+    Right c -> fmap Right <$> c2d c
+
+
+
 instance (Reflex t, MonadHold t m) => Profunctor (Step t m) where
   dimap f g (Step a2b) = fmap g $ mkStep (a2b . f)
+
+
 
 instance (Functor m, Reflex t, Arrow (Step t m)) => Apply (Step t m a) where
   liftF2 f a2b a2c = fmap (uncurry f) $ a2b &&& a2c
@@ -74,6 +89,22 @@ instance (Functor m, Reflex t, Arrow (Step t m)) => Applicative (Step t m a) whe
   pure = arr . const
   liftA2 = liftF2
 
+instance (Adjustable t m, MonadHold t m, PostBuild t m) => Bind (Step t m a) where
+  join (Step a2sb) = mkStep $ \a -> do
+    evSb <- a2sb a
+    fmap (switch . current) $ networkHold (pure never) $ ffor evSb $ \(Step a2b) -> a2b a
+
+instance (Adjustable t m, MonadHold t m, PostBuild t m) => Monad (Step t m a) where
+  x >>= f = join (fmap f x)
+
+
+instance (Reflex t, Applicative m, MonadHold t m) => Align (Step t m a) where
+  nil = Step $ const $ pure nil
+#if MIN_VERSION_these(0, 8, 0)
+instance (Reflex t, Apply m) => Semialign (Step t m a) where
+#endif
+  align (Step a2b) (Step a2c) = mkStep $ (liftA2 . liftA2) align a2b a2c
+
 instance (Apply m, Reflex t, MonadHold t m) => Alt (Step t m a) where
   Step f <!> Step g = mkStep $ (liftF2 . liftF2) (<!>) f g
 
@@ -81,30 +112,10 @@ instance (Apply m, Applicative m, Reflex t, MonadHold t m, Arrow (Step t m)) => 
   empty = nil
   (<|>) = (<!>)
 
-
-instance (Adjustable t m, MonadHold t m, PostBuild t m) => Bind (Step t m a) where
-  join (Step a2sb) = mkStep $ \a -> do
-    evSb <- a2sb a
-    fmap (switch . current) $ networkHold (pure never) $ ffor evSb $ \(Step a2b) -> a2b a
-
-
-#if MIN_VERSION_these(0, 8, 0)
-instance (Reflex t, Applicative m) => Align (Step t m a) where
-  nil = Step $ const $ pure never
-instance (Reflex t, Apply m) => Semialign (Step t m a) where
-  align (Step a2b) (Step a2c) = Step $ (liftA2 . liftA2) align a2b a2c
-#else
-instance (Reflex t, Applicative m) => Align (Step t m a) where
-  nil = Step $ const $ pure never
-  align (Step a2b) (Step a2c) = Step $ (liftA2 . liftA2) align a2b a2c
-#endif
-
-instance (Semigroup b, Reflex t, Applicative m) => Semigroup (Step t m a b) where
-  f <> g = alignWith (mergeThese (<>)) f g
---  sconcat = fmap sconcat . mergeList . toList
+instance (Semigroup b, Reflex t, Adjustable t m, MonadHold t m, PostBuild t m) => Semigroup (Step t m a b) where
+  f <> g = liftF2 (<>) f g
   stimes n = fmap $ stimes n
 
-instance (Semigroup b, Reflex t, Applicative m) => Monoid (Step t m a b) where
+instance (Semigroup b, Reflex t, Adjustable t m, MonadHold t m, PostBuild t m) => Monoid (Step t m a b) where
   mempty = nil
   mappend = (<>)
---  mconcat = fmap sconcat . mergeList
