34e404a0e65805c0815cbb27fbcd6a212c7b9091
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-07-24 22:16:23 +0100

    Patch 'foundation' package on ghcjs on mac

--- skeleton/default.nix
+++ skeleton/default.nix
@@ -1,4 +1,5 @@
-{ obelisk ? import ./.obelisk/impl {
+{ system ? builtins.currentSystem
+, obelisk ? import ./.obelisk/impl {
     system = builtins.currentSystem;
     iosSdkVersion = "10.2";
     # You must accept the Android Software Development Kit License Agreement at
@@ -8,9 +9,22 @@
   }
 }:
 with obelisk;
-project ./. ({ ... }: {
+project ./. ({ pkgs, ... }@args: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+
+  overrides = pkgs.lib.foldr pkgs.lib.composeExtensions  (_: _: {}) [
+    (self: super: {
+      foundation =
+                 if ! self.ghc.isGhcjs then self.foundation
+                 else pkgs.haskell.lib.overrideCabal super.foundation (drv: {
+        postPatch = (drv.postPatch or "") + pkgs.lib.optionalString (system == "x86_64-darwin") ''
+          substituteInPlace foundation.cabal --replace 'if os(linux)' 'if os(linux) && !impl(ghcjs)'
+          substituteInPlace foundation.cabal --replace 'if os(osx)' 'if os(linux) && impl(ghcjs)'
+        '';
+      });
+    })
+  ];
 })
