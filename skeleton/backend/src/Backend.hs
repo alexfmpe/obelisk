8195809e955fe0b078c436f0d51597bc77bcbcef
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-10-05 19:40:52 +0100

    Test inline-c in 'ob run'

--- skeleton/backend/src/Backend.hs
+++ skeleton/backend/src/Backend.hs
@@ -1,13 +1,22 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
+
 module Backend where
 
 import Common.Route
 import Obelisk.Backend
+import qualified Language.C.Inline as C
+
+C.include "<math.h>"
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> serve $ const $ return ()
+  { _backend_run = \serve -> do
+      putStrLn . show =<< [C.exp| double{ cos(1) } |]
+      putStrLn . show =<< [C.exp| double{ sin(1) } |]
+      serve $ const $ return ()
   , _backend_routeEncoder = fullRouteEncoder
   }
