07e87847a9a13ebe131a934a43157097398fcece
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-11-25 09:36:20 +0000

    Revert "Oh right, jsaddle-warp"
    This reverts commit 9f08951358f8c6837c156e265fd54688c282f3be.

--- skeleton/frontend/src/Frontend.hs
+++ skeleton/frontend/src/Frontend.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
 module Frontend where
 
+import Control.Monad.IO.Class
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Obelisk.Frontend
@@ -14,10 +17,17 @@ import Common.Api
 import Common.Route
 import Obelisk.Generated.Static
 
+import qualified Language.C.Inline as C
+
+C.include "<math.h>"
+
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
   { _frontend_head = el "title" $ text "Obelisk Minimal Example"
-  , _frontend_body = do
+  , _frontend_body = prerender_ blank $ do
+      el "div" . text . T.pack . show =<< liftIO [C.exp| double{ cos(1) } |]
+      el "div" . text . T.pack . show =<< liftIO [C.exp| double{ sin(1) } |]
+
       text "Welcome to Obelisk!"
       el "p" $ text $ T.pack commonStuff
       elAttr "img" ("src" =: static @"obelisk.jpg") blank
