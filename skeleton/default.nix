deb8505101f1bc2dfc5c8860b7c83b8002104487
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-04-05 21:00:56 +0100

    Try with recent inline-c

--- skeleton/default.nix
+++ skeleton/default.nix
@@ -8,9 +8,17 @@
   }
 }:
 with obelisk;
-project ./. ({ ... }: {
+project ./. ({ pkgs, ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+
+  overrides = self: super: {
+    inline-c = self.callHackageDirect {
+      pkg = "inline-c";
+      ver = "0.9.1.0";
+      sha256 = "0phr840cjkb2hj4hvgrrfxqj1zfgy204qa1h3mnhnag8fi2d10ly";
+    } {};
+  };
 })
