1822e0ed2149e599ca05c6968111b00ef37b6bf9
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-05-31 03:58:52 +0100

    adjustable

--- skeleton/frontend/src/Frontend.hs
+++ skeleton/frontend/src/Frontend.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Frontend where
@@ -18,6 +23,7 @@ import Data.Tree
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Language.Javascript.JSaddle (eval, liftJSM)
+import Unsafe.Coerce
 
 import Prelude hiding (div, head)
 
@@ -58,61 +64,124 @@ frontend = Frontend
 
       prerender_ blank $ do
         html <- root
-        tree <- drawTree <$> currentTree html
-        el "pre" . text . T.pack $ tree
+        tick <- tickLossyFromPostBuildTime 0.3
+        widgetHold_ blank $ ffor tick $ \_ -> do
+          tree <- drawTree <$> currentTree html
+          el "pre" . text . T.pack $ tree
 
       return ()
   }
 
 {- Example -}
-example :: (MonadFix m, MonadIO m, MonadHold t m, Reflex t) => DOMNode -> m (Dynamic t Int)
+example
+  :: (MonadFix m, MonadIO m, MonadIO (Performable m), MonadHold t m, PerformEvent t m, PostBuild t m, TriggerEvent t m)
+  => DOMNode m -> m (Dynamic t Int)
 example parent = do
   div <- el_ parent "div" mempty
   _ <- el_ div "input" mempty
-  _ <- gallery div
 
   but1 <- el_ parent "button" $ "background" =: "blue"
   but2 <- el_ div "button" $ "background" =: "red"
-  clicks <- foldDyn (+) 0 $ 1 <$ leftmost [click but1, click but2]
+  let clickEv = leftmost [click but1, click but2]
+  clicks <- foldDyn (+) 0 $ 1 <$ clickEv
+
+
+  let
+    w0 anchor cast = do
+      -- el_ parent "MUST NOT TYPECHECK" mempty --     • Couldn't match type ‘localHierarchy’ with ‘m’
+      p <- anchor "div" ("placeholder" =: "true")
+      fmap cast $ el_ p "div" ("placeholder" =: "true")
+
+    wEv anchor b = do
+      when b $ gallery anchor
+      -- el_ parent "MUST NOT TYPECHECK" mempty --     • Couldn't match type ‘m’ with ‘Performable m’
+      pure b
+
+  tick <- tickLossyFromPostBuildTime 1
+  tg <- toggle True tick
+  (_n0, _nEv) <- elAdjustable_ parent w0 $ \anchor -> ffor (updated tg) $ wEv anchor
+
   pure clicks
 
-gallery :: MonadIO m => DOMNode -> m [DOMNode]
-gallery parent = do
-  for ["A", "B", "C"] $ \src -> do
-    el_ parent "img" $ "src" =: (src <> ".png")
+gallery :: MonadIO m => Anchor m h -> m ()
+gallery anchor = do
+  for_ ["A", "B", "C"] $ \src -> do
+    anchor "img" $ "src" =: (src <> ".png")
 
-root :: (MonadFix m, MonadIO m, MonadHold t m, Reflex t) => m DOMNode
+root
+  :: (MonadFix m, MonadIO m, MonadIO (Performable m), MonadHold t m, PerformEvent t m, PostBuild t m, TriggerEvent t m)
+  => m (DOMNode m)
 root = mkRoot $ \(_h,b) -> example b
 
 {- Stubs -}
 type Tag = Text
-type Children = Seq DOMNode
+type Children h = Seq (IORef (Seq (DOMNode h)))
 type Attributes = Map Text Text
+type Anchor m h = Tag -> Attributes -> m (DOMNode h)
 
-data DOMNode = DOMNode Tag (IORef Attributes) (IORef Children)
+data DOMNode h = DOMNode Tag (IORef Attributes) (IORef (Children h))
+newtype Coercer a = Coercer a
 
-click :: Reflex t => DOMNode -> Event t ()
+click :: Reflex t => DOMNode h -> Event t ()
 click _ = never
 
-el_ :: MonadIO m => DOMNode -> Tag -> Map Text Text -> m DOMNode
-el_ (DOMNode _ _ childrenRef) tag' attrs = liftIO $ do
-  n <- DOMNode tag' <$> newIORef attrs <*> newIORef mempty
-  modifyIORef childrenRef (|> n)
+el_ :: MonadIO m => DOMNode m -> Anchor m m
+el_ (DOMNode _ _ childrenRef) tag' attrs = do
+  n <- mkNode tag' attrs
+  liftIO $ do
+    ref <- newIORef $ singleton n
+    modifyIORef childrenRef (|> ref)
   pure n
 
-mkRoot :: MonadIO m => ((DOMNode, DOMNode) -> m a) -> m DOMNode
+elAdjustable_
+  :: (MonadIO m, MonadIO n, MonadIO (Performable m), PerformEvent t m)
+  => DOMNode m
+  -> (
+     forall localHierarchy                    -- name for error messages
+     .  MonadIO localHierarchy
+     => Anchor localHierarchy localHierarchy  -- root of local hierarchy
+     -> (DOMNode localHierarchy -> DOMNode m) -- cast for returning nodes
+     -> localHierarchy a
+     )
+  -> (Anchor n m -> Event t (Performable m b))
+  -> m (a, Event t b)
+elAdjustable_ (DOMNode _ _ childrenRef) w0 wEv = do
+  slice <- liftIO $ newIORef mempty
+  liftIO $ modifyIORef childrenRef (|> slice)
+
+  let
+    castNode = unsafeCoerce
+
+    anchor t a = do
+      n <- fmap castNode $ mkNode t a
+      liftIO $ modifyIORef slice (|> n)
+      pure n
+
+  n0 <- w0 anchor castNode
+
+  ev' <- performEvent $ ffor (wEv anchor) $ \x -> do
+    liftIO $ writeIORef slice mempty
+    x
+
+  pure (n0, ev')
+
+mkRoot :: MonadIO m => ((DOMNode m, DOMNode m) -> m a) -> m (DOMNode m)
 mkRoot f = do
-  html <- liftIO $ DOMNode "html" <$> newIORef mempty <*> newIORef mempty
+  html <- mkNode "html" mempty
   h <- el_ html "head" mempty
   b <- el_ html "body" mempty
   _ <- f (h,b)
   pure html
 
+mkNode :: MonadIO m => Tag -> Map Text Text -> m (DOMNode m)
+mkNode tag' attrs = liftIO $ DOMNode tag' <$> newIORef attrs <*> newIORef mempty
+
+
 {- Debug -}
-currentTree :: MonadIO m => DOMNode -> m (Tree String)
+currentTree :: MonadIO m => DOMNode h -> m (Tree String)
 currentTree (DOMNode tag' attrsRef childrenRef) = liftIO $ do
   attrs <- readIORef attrsRef
-  children <- readIORef childrenRef
+  children <- traverse readIORef =<< readIORef childrenRef
   let keyvalue (k,v) = k <> if T.null v
                             then ""
                             else "=\"" <> v <> "\""
@@ -125,4 +194,4 @@ currentTree (DOMNode tag' attrsRef childrenRef) = liftIO $ do
         , ">"
         ]
 
-  Node attrs' <$> traverse currentTree (toList children)
+  Node attrs' . toList <$> traverse currentTree (join children)
