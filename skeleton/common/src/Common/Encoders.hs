23d736f5b52508944ed4094e9ec4d71d68e23c98
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-12-19 23:12:23 +0000

    Reify decode failure in hask

--- skeleton/common/src/Common/Encoders.hs
+++ skeleton/common/src/Common/Encoders.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeOperators #-}
 module Common.Encoders where
@@ -23,14 +28,18 @@ import qualified Data.Binary.Put as Binary
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as ByteString
 import Data.Foldable
+import Data.Functor.Compose
+import Data.Functor.Contravariant
 import Data.Functor.Identity
+import Data.Int
+import Data.List
 import Data.HexString
 import Data.Semigroupoid
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Word
+import GHC.Generics (Generic)
 import Prelude hiding (id, (.))
-
 type SomeData = Either () () :. Word8 :. Either () Word8 :. [Word8]
 
 test :: IO ()
@@ -50,14 +59,14 @@ test = do
 
 
     b = encodeHask f a
-    c = decodeHask f b
+    c = decodeHask f `runParse` b
   print a
   print b
   print $ fromBytes $ ByteString.toStrict b
   print c
   putStrLn ""
-  putStrLn $ T.unpack $ decideSolidity f
-  putStrLn $ T.unpack $ decodeRust f
+--  putStrLn $ T.unpack $ decideSolidity f
+--  putStrLn $ T.unpack $ decodeRust f
 
 
 type (:.) = (,)
@@ -73,12 +82,28 @@ infixr 6 :+:
 pattern (:+:) :: Format a -> Format b -> Format (Either a b)
 pattern a :+: b = Format_Sum a b
 
+data HList (f :: * -> *) (types :: [*]) where
+  HNil :: HList f '[]
+  HCons :: f x -> HList f xs -> HList f (x ': xs)
+
+data Labeled a = Labeled Text a
+  deriving (Eq, Ord, Show, Generic)
+
+type Fields = HList Labeled
+
 data Format a where
   Format_Magic :: ByteString -> Format ()
   Format_Byte :: Format Word8
   Format_Product :: Format a -> Format b -> Format (a,b)
   Format_Sum :: Format a -> Format b -> Format (Either a b)
   Format_Replicate :: Format Word8 -> Format a -> Format [a]
+--  Format_Struct :: HList (Compose Labeled Format) types -> Format (HList Format types)
+
+field :: Text -> Format a -> HList (Compose Labeled Format) as -> HList (Compose Labeled Format) (a ': as)
+field lbl f = HCons (Compose (Labeled lbl f))
+
+struct :: HList f '[]
+struct = HNil
 
 optional :: Format a -> Format (Either () a)
 optional f = Format_Sum epsilon f
@@ -103,19 +128,19 @@ instance (Category decode, Category encode) => Category (EncoderImpl decode enco
     (_encoderImpl_encode f <<< _encoderImpl_encode g)
 
 data Categoryish_ImplicitSource m c a b where
-  Categoryish_ImplicitSource  :: (forall x. m x -> c a x) -> m b -> Categoryish_ImplicitSource m c a b
+  Categoryish_ImplicitSource :: (forall x. m x -> c a x) -> m b -> Categoryish_ImplicitSource m c a b
 data Categoryish_ImplicitTarget m c a b where
   Categoryish_ImplicitTarget :: (forall x. m x -> c x b) -> m a -> Categoryish_ImplicitTarget m c a b
 
 instance Semigroupoid c => Semigroupoid (Categoryish_ImplicitSource m c) where
-  Categoryish_ImplicitSource fbc mc `o` Categoryish_ImplicitSource fab mb =
-    Categoryish_ImplicitSource (\m -> fbc m `o` fab mb) mc
+  Categoryish_ImplicitSource b2c mc `o` Categoryish_ImplicitSource a2b mb =
+    Categoryish_ImplicitSource (\m -> b2c m `o` a2b mb) mc
 instance Semigroupoid c => Semigroupoid (Categoryish_ImplicitTarget m c) where
-  Categoryish_ImplicitTarget fbc mb `o` Categoryish_ImplicitTarget fab ma =
-    Categoryish_ImplicitTarget (\m -> fbc mb `o` fab m) ma
+  Categoryish_ImplicitTarget b2c mb `o` Categoryish_ImplicitTarget a2b ma =
+    Categoryish_ImplicitTarget (\m -> b2c mb `o` a2b m) ma
 
-newtype Putish a = Putish { unPutish :: a -> Binary.Put }
-newtype Getish a = Getish { unGetish :: Binary.Get a }
+type DecodeViaGet = Categoryish_ImplicitSource Binary.Get      Parse
+type EncodeViaPut = Categoryish_ImplicitTarget (Op Binary.Put) (->)
 
 decode :: Encoder Identity decode encode decoded encoded -> decode encoded decoded
 decode (Encoder (Identity impl)) = _encoderImpl_decode impl
@@ -123,18 +148,19 @@ decode (Encoder (Identity impl)) = _encoderImpl_decode impl
 encode :: Encoder Identity decode encode decoded encoded -> encode decoded encoded
 encode (Encoder (Identity impl)) = _encoderImpl_encode impl
 
-encodeHask :: Format a -> (a -> ByteString)
+encodeHask :: Format a -> (->) a ByteString
 encodeHask f = Binary.runPut . encodeToPut (encode $ encoderHask f)
 
-decodeHask :: Format a -> (ByteString -> a)
-decodeHask f = Binary.runGet $ encodeToGet (decode $ encoderHask f)
+decodeHask :: Format a -> Parse ByteString a
+decodeHask f = case decode (encoderHask f) of
+  Categoryish_ImplicitSource bs2a m -> bs2a m
 
 encoderHask
   :: Applicative check
   => Format a
   -> Encoder check
-      (Categoryish_ImplicitSource Getish (->))
-      (Categoryish_ImplicitTarget Putish (->))
+      DecodeViaGet
+      EncodeViaPut
       a
       ByteString
 encoderHask = \case
@@ -144,8 +170,9 @@ encoderHask = \case
     }
   Format_Magic bs -> unsafeMkEncoder $ EncoderImpl
     { _encoderImpl_decode = decodeFromGet $ do
+        --TODO: only consume until mismatch?
         bs' <- Binary.getByteString (fromIntegral $ ByteString.length bs)
-        when (bs' /= ByteString.toStrict bs) $ error "derp"
+        when (bs' /= ByteString.toStrict bs) $ fail "Magic number mismatch"
     , _encoderImpl_encode = encodeFromPut $ \() -> Binary.putByteString $ ByteString.toStrict bs
     }
   Format_Product fa fb -> Encoder $ do
@@ -156,8 +183,8 @@ encoderHask = \case
           encodeToPut (_encoderImpl_encode ea) a
           encodeToPut (_encoderImpl_encode eb) b
       , _encoderImpl_decode = decodeFromGet $ do
-          a <- encodeToGet (_encoderImpl_decode ea)
-          b <- encodeToGet (_encoderImpl_decode eb)
+          a <- decodeToGet (_encoderImpl_decode ea)
+          b <- decodeToGet (_encoderImpl_decode eb)
           pure (a,b)
       }
   Format_Sum fa fb -> Encoder $ do
@@ -169,9 +196,9 @@ encoderHask = \case
           Right b -> Binary.putWord8 1 *> encodeToPut (_encoderImpl_encode eb) b
       , _encoderImpl_decode = decodeFromGet $ do
           Binary.getWord8 >>= \case
-            0 -> Left <$> encodeToGet (_encoderImpl_decode ea)
-            1 -> Right <$> encodeToGet (_encoderImpl_decode eb)
-            _ -> error "derp"
+            0 -> Left <$> decodeToGet (_encoderImpl_decode ea)
+            1 -> Right <$> decodeToGet (_encoderImpl_decode eb)
+            _ -> fail "Invalid tag for sum type"
       }
   Format_Replicate fn fa -> Encoder $ do
     en <- unEncoder $ encoderHask fn
@@ -181,35 +208,31 @@ encoderHask = \case
           encodeToPut (_encoderImpl_encode en) (fromIntegral $ length as)
           for_ as $ encodeToPut (_encoderImpl_encode ea)
       , _encoderImpl_decode = decodeFromGet $ do
-          n <- encodeToGet (_encoderImpl_decode en)
-          replicateM (fromIntegral n) (encodeToGet (_encoderImpl_decode ea))
+          n <- decodeToGet (_encoderImpl_decode en)
+          replicateM (fromIntegral n) (decodeToGet (_encoderImpl_decode ea))
       }
 
-decodeFromGet :: forall x. Binary.Get x -> Categoryish_ImplicitSource Getish (->) ByteString x
-decodeFromGet = Categoryish_ImplicitSource (\g -> Binary.runGet (unGetish g)) . Getish
+decodeFromGet :: Binary.Get a -> DecodeViaGet ByteString a
+decodeFromGet = Categoryish_ImplicitSource $ \g -> Parse $ \bs -> case Binary.runGetOrFail g bs of
+  Left (rest, cursor, err) -> Left (rest, cursor, T.pack err)
+  Right (_, _, res) -> Right res
 
-encodeFromPut :: forall x. (x -> Binary.Put) -> Categoryish_ImplicitTarget Putish (->) x ByteString
-encodeFromPut = Categoryish_ImplicitTarget (\p -> Binary.runPut . unPutish p) . Putish
+encodeFromPut :: (a -> Binary.Put) -> EncodeViaPut a ByteString
+encodeFromPut = Categoryish_ImplicitTarget (\p -> Binary.runPut . getOp p) . Op
 
-encodeToPut :: forall x. Categoryish_ImplicitTarget Putish (->) x ByteString -> (x -> Binary.Put)
-encodeToPut (Categoryish_ImplicitTarget _ (Putish p)) = p
+encodeToPut :: EncodeViaPut a ByteString -> (a -> Binary.Put)
+encodeToPut (Categoryish_ImplicitTarget _ p) = getOp p
 
-encodeToGet :: forall x. Categoryish_ImplicitSource Getish (->) ByteString x -> Binary.Get x
-encodeToGet (Categoryish_ImplicitSource _ (Getish g)) = g
+decodeToGet :: DecodeViaGet ByteString a -> Binary.Get a
+decodeToGet (Categoryish_ImplicitSource _ g) = g
 
-
-newtype Parse parsed a b = Parse { unParse :: a -> parsed b }
-instance Monad parsed => Category (Parse parsed) where
+newtype Parse a b = Parse { runParse :: a -> Either (ByteString, Int64, Text) b }
+instance Category Parse where
   id = Parse pure
-  f . g = Parse $ unParse f <=< unParse g
-instance Monad parsed => Arrow (Parse parsed) where
+  f . g = Parse $ runParse f <=< runParse g
+instance Arrow Parse where
   arr f = Parse $ pure . f
   first (Parse f) = Parse $ \(a,b) -> fmap (,b) (f a)
-
-parseBinary :: Binary.Get a -> ByteString -> Either Text a
-parseBinary x bs = case Binary.runGetOrFail x bs of
-  Left (_rest, _, err) -> Left (T.pack err)
-  Right (_rest, _, a) -> Right a
 
 decodeRust :: Format a -> Text
 decodeRust f = snd $ runWriter $ flip runReaderT 0 $ do
