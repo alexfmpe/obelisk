faf8f7a7575120725b698ab59bab3c433ca5d74a
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2021-05-21 14:07:43 +0100

    update

--- skeleton/frontend/src/Frontend.hs
+++ skeleton/frontend/src/Frontend.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -14,8 +13,9 @@
 
 module Frontend where
 
-import Control.Monad (when, (<=<))
-import Control.Monad.Cont
+import Control.Monad (join, when, (<=<))
+import Control.Monad.Trans
+import Control.Monad.Trans.Cont
 import Control.Monad.Fix (MonadFix)
 import Data.Functor (void)
 import Data.Functor.Bind (Apply(..))
@@ -70,7 +70,7 @@ instance Monad m => Apply (RoutedT t r m) where
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
   { _frontend_head = el "title" $ text "Workflow examples"
-  , _frontend_body = do
+  , _frontend_body = prerender_ blank $ do
       let
         example name w = do
           el "strong" $ text name
@@ -90,13 +90,15 @@ frontend = Frontend
         choice = choiceStyle $ const mempty
         choiceFade = choiceStyle $ \x -> if x == "_" then "opacity:0.5;" else mempty
 
-        choiceStyle style x = elAttr "div" ("style" =: ("display:flex; margin-right: 15px;" <> style x)) $ do
+        buttons style = elAttr "div" ("style" =: ("display:flex; margin-right: 15px;" <> style))
+
+        choiceStyle style x = buttons (style x) $ do
           a <- btn $ x <> ".A"
           b <- btn $ x <> ".B"
           br
           pure $ leftmost [a,b]
 
-        choiceFork x = el "div" $ do
+        choiceFork x = buttons "" $ do
           a <- btn $ x <> ".A"
           b <- btn $ x <> ".B"
           ab <- btn $ x <> ".*"
@@ -104,70 +106,70 @@ frontend = Frontend
           br
           pure $ leftmost [a,b,ab,z]
 
-        controls = do
+        controls = buttons "" $ do
           yes <- button "Replay"
           no <- button "Continue"
           stopEv <- button "Stop"
           br
           pure $ leftmost [Just True <$ yes, Just False <$ no, Nothing <$ stopEv]
 
-        choices mkControls mkLayer = do
-          start <- replay
-          x0 <- mkLayer "0"
+        choices mkControls mkLayer = evalContT $ do
+          start <- label_
+          x0 <- lift $ mkLayer "0"
           callCC $ \exit -> do
-            x1 <- mkLayer x0
-            mkControls >>= \case
+            x1 <- lift $ mkLayer x0
+            lift mkControls >>= \case
               Just True -> start
               Just False -> pure ()
-              Nothing -> stop --exit "X"
-            x2 <- mkLayer x1
-            x3 <- mkLayer x2
+              Nothing -> lift stop --exit "X"
+            x2 <- lift $ mkLayer x1
+            x3 <- lift $ mkLayer x2
             pure x3
 
-        machineInstant a ma = machine $ do
+        stepInstant a ma = step $ do
           pb <- getPostBuild
           ev <- ma
           pure $ leftmost [a <$ pb, ev]
 
       example "Wizard" $
         justShow <=< wizard $ choices
-          (machine controls)
-          (machine . choice)
+          (step controls)
+          (step . choice)
 
       example "Stack" $
         justShow <=< stack $ choices
-          (machine controls)
-          (machine . choice)
+          (step controls)
+          (step . choice)
+{-
+      example "Breadth First" $
+        justShow <=< breadthFirst $ choices
+          (step controls)
+          (step . choice)
 
-      example "BFS" $
-        justShow <=< bfs $ choices
-          (machine controls)
-          (machine . choice)
-
-      example "DFS" $
-        justShow <=< dfs $ choices
-          (machine controls)
-          (machine . choice)
-
+      example "Depth First" $
+        justShow <=< depthFirst $ choices
+          (step controls)
+          (step . choice)
+-}
       example "Wizard: postBuild" $
         justShow <=< wizard $ pure "_"
 
       example "Stack: postBuild" $
         justShow <=< stack $ choices
-          (machine controls)
-          (machineInstant "_" . choiceFade)
+          (step controls)
+          (stepInstant "_" . choiceFade)
 
       example "Wizard of stacks" $ do
         justShow <=< wizard $ do
-          x <- machine $ stack $ choices (pure $ Just False) $ machine . choice
-          y <- machine $ stack $ choices (pure $ Just False) $ machine . choice
+          x <- step $ stack $ choices (pure $ Just False) $ step . choice
+          y <- step $ stack $ choices (pure $ Just False) $ step . choice
           pure (x,y)
 
       example "Stack of workflows" $ mdo
         ymd <- stack $ do
-          y <- machine . workflowView $ year clk 2000
-          m <- machine . workflowView $ month clk January
-          d <- machine . workflowView $ day clk y m 27
+          y <- step . workflowView $ year clk 2000
+          m <- step . workflowView $ month clk January
+          d <- step . workflowView $ day clk y m 27
           pure (y,m,d)
         widgetHold blank $ ffor ymd $ \(y,m,d) -> text $ T.intercalate "-" [tshow y, tshow m, tshow d]
         br
@@ -179,29 +181,46 @@ frontend = Frontend
           br
           button "trigger all simultaneously"
         pure ()
+{-
+      example "Round robin wizards" $ do
+        justShow <=< wizard . uncurry roundRobin . join (,) $ choices
+          (step controls)
+          (step . choice)
 
+      let ad = fmap void (button "Buy now!") <* br
+
+      example "Before wizard" $ do
+        justShow <=< wizard . before ad $ choices
+          (step controls)
+          (step . choice)
+
+      example "After wizard" $ do
+        justShow <=< wizard . after ad $ choices
+          (step controls)
+          (step . choice)
+-}
       example "Forking the timelines" $ do
         let
-          choices' mkControls mkLayer mkFork = do
-            start <- replay
+          choices' mkControls mkLayer mkFork = evalContT $ do
+            start <- label_
             x0 <- mkFork "0"
             x0' <- case x0 of
-              "Stop" -> stop
-              x | ".*" `T.isSuffixOf` x -> fork ["0.A", "0.B"]
+              "Stop" -> lift stop
+--              x | ".*" `T.isSuffixOf` x -> fork ["0.A", "0.B"]
               x -> pure x
             x1 <- mkLayer x0
             mkControls >>= \case
               Just True -> start
               Just False -> pure ()
-              Nothing -> stop
+              Nothing -> lift stop
             x2 <- mkLayer x1
             x3 <- mkLayer x2
             pure x3
 
-        let c = choices' (machine controls) (machine . choice) (machine . choiceFork)
+        let c = choices' (lift $ step controls) (lift . step . choice) (lift . step . choiceFork)
 
         example "Wizard fork" $ justShow <=< wizard $ c
         example "Stack fork" $ justShow <=< stack $ c
-        example "BFS fork" $ justShow <=< bfs $ c
-        example "DFS fork" $ justShow <=< dfs $ c
+--        example "Breadth First fork" $ justShow <=< breadthFirst $ c
+--        example "Depth First fork" $ justShow <=< depthFirst $ c
   }
