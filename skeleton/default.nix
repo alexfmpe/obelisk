8c2882063559c62e6aa79acf7c1c68f72f649d61
Alexandre Esteves <alexfmpe@proton.me>
2024-03-13 21:18:11 +0000

    Add partially working HLS to skeleton shell, disabled by default

--- skeleton/default.nix
+++ skeleton/default.nix
@@ -15,9 +15,17 @@
   }
 }:
 with obelisk;
-project ./. ({ ... }: {
+project ./. ({ pkgs, ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+
+  shellToolOverrides = self: super: {
+    # Very fragile, works sometimes for some of the packages declared in hie.yaml
+    # There are currently no guarantees of support whatsoever
+    # Uncomment at your own risk
+    # To use, run your editor inside `ob shell`
+    # haskell-language-server = assert (self.ghc.version == "8.10.7"); pkgs.haskell.packages.ghc8107.haskell-language-server;
+  };
 })
